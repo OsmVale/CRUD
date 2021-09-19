18d770fec2f2cd3bfa6e3333c7002d9241abe326
OsmVale <ovalenzuelad@miumg.edu.gt>
2021-09-18 22:39:24 -0600

    make it better

---
+++ configurations/db-conf.js
@@ -0,0 +1,9 @@
+const host= "nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+const user= "fu4ve1npbw1g3fui";
+const database= "so9dke3aq3zsmrpe";
+const password= "sciazx4j7f7foxqj";
+
+exports.password=password;
+exports.host=host;
+exports.user=user;
+exports.database=database;
